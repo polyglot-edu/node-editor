553ac1bc065759d55a865a4a08738da421c857de
daviFrag <davide.frag@gmail.com>
2022-10-25 19:48:08 +0200

    add path upload + refactor darkmode

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,24 +3,18 @@ import { ThemeProvider } from '@fluentui/react';
 import { Toaster } from 'react-hot-toast';
 import AppFlowProvider from '../AppFlowProvider/AppFlowProvider';
 import { API } from '../../data/api';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import UploadForm from '../UploadForm/UploadForm';
 
 const App = () => {
-    const [theme, setTheme] = useState<"dark" | "light">("light");
 
     useEffect(() => {
-        function mediaListener(e: MediaQueryListEvent) {
-            setTheme(e.matches ? "dark" : "light");
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
         }
-
-        // Setup dark/light mode for the first time
-        setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-
-        // Add listener to update styles
-        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", mediaListener);
-
-        // Remove listener
-        return () => window.matchMedia("(prefers-color-scheme: dark)").removeEventListener("change", mediaListener);
     }, []);
 
     return (
@@ -29,13 +23,15 @@ const App = () => {
                 <Toaster
                     position='bottom-center'
                     toastOptions={{
-                        style: {
-                            background: theme === "dark" ? "#333" : "#fff",
-                            color: theme === "dark" ? '#fff' : "#363636",
-                        }
+                        className: "bg-white dark:bg-[#333] text-white dark:text-[#363636]",
+                        // style: {
+                        //     background: theme === "dark" ? "#333" : "#fff",
+                        //     color: theme === "dark" ? '#fff' : "#363636",
+                        // }
                     }}
                 />
                 <Routes>
+                <Route path="/upload" element={<UploadForm fetchFunction={API.createNewFlow}/>} />
                     <Route path="/id/:flowId" element={<AppFlowProvider fetchFunction={API.loadFlowElementsAsync} />} />
                     <Route path="/example/:flowId" element={<AppFlowProvider fetchFunction={API.loadExampleFlowElementsAsync} canSaveFlow={false} />} />
                     <Route index element={<Navigate to="/example/1" replace />} />
