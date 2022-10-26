0068e4e3c2501b68890d13abbfb0143aa58857e6
daviFrag <davide.frag@gmail.com>
2022-10-26 20:33:19 +0200

    fix multiple registration of fluentui icons + add reportwebvitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,18 @@ import "../components/DrawingArea.css"
 import "../components/Card.css"
 
 import type { AppProps } from 'next/app'
-
+import reportWebVitals from '../reportWebVitals'
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
 
-initializeIcons();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+
+initializeIcons();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
