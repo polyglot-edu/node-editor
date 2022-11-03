1c3f43ec272d4570c908956bf432f6a560828780
daviFrag <davide.frag@gmail.com>
2022-11-03 18:14:14 +0100

    fix: backend url broken

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-const CURRENT_HOST = process.env.HEROKU_APP_NAME ? "https://" + process.env.HEROKU_APP_NAME : "http://localhost:3000";
+const CURRENT_HOST = process.env.HEROKU_APP_NAME ? "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com" : "http://localhost:3000";
 const BACK_URL = process.env.BACK_URL || "http://localhost:5000";
 
 module.exports = {
