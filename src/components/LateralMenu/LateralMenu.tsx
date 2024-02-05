0ea4a86767c9443d9401bc7a55a652559906ba46
tmaog <tommasoguidolin01@gmail.com>
2024-02-05 11:02:46 +0100

    fix: fix

--- src/components/LateralMenu/LateralMenu.tsx
+++ src/components/LateralMenu/LateralMenu.tsx
@@ -73,6 +73,7 @@ const listImplementedNodes = [
   'SummaryNode',
   'codingQuestionNode',
   'openQuestionNode',
+  'PromptEngineeringNode',
 ];
 export type LateralMenuProps = {
   isOpen: boolean;
