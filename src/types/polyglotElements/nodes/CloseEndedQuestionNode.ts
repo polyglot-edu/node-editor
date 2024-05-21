e616b724e0bcff17457496a6dc3d8d460efce713
tmaog <tommasoguidolin01@gmail.com>
2024-05-21 13:51:49 +0200

    fix: graphic upgrade based on testing

--- src/types/polyglotElements/nodes/CloseEndedQuestionNode.ts
+++ src/types/polyglotElements/nodes/CloseEndedQuestionNode.ts
@@ -6,7 +6,8 @@ import { defaultPolyglotNodeData, NodeData, PolyglotNode } from './Node';
 
 export type CloseEndedQuestionNodeData = NodeData & {
   question: string;
-  correctAnswers: string[];
+  correctAnswers: string[];  
+  textToFill?: string;
   isAnswerCorrect: boolean[];
 };
 
