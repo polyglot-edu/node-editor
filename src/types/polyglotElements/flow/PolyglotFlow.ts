de34d54cd7edcf3163b9a043a4b8d744a186258f
tmaog <tommasoguidolin01@gmail.com>
2024-05-22 10:52:46 +0200

    fix: api update

--- src/types/polyglotElements/flow/PolyglotFlow.ts
+++ src/types/polyglotElements/flow/PolyglotFlow.ts
@@ -58,7 +58,7 @@ export type AIExerciseType = {
   macroSubject?: string;
   title?: string;
   level: number; //0=primary_school, 1=middle_school, 2=high_school, 3=college, 4=academy
-  typeOfExercise: TypeOfExercise; //0=fill_in_the_blanks, 1=question, 2=choice, 3=conceptual, 4=practical
+  typeOfActivity: TypeOfExercise; //0=fill_in_the_blanks, 1=question, 2=choice, 3=conceptual, 4=practical
   learningObjective?: string;
   bloomLevel: number; //0=Remembering, 1=Understanding, 2=Applying, 3=Analyzing, 4=Evaluating, 5=Creating
   language: string;
