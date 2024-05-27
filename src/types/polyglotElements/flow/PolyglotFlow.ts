ea1cc3a4d3f32f8339df61f31fe9091526026bf9
tmaog <tommasoguidolin01@gmail.com>
2024-05-27 14:22:55 +0200

    feat: new types

--- src/types/polyglotElements/flow/PolyglotFlow.ts
+++ src/types/polyglotElements/flow/PolyglotFlow.ts
@@ -25,50 +25,27 @@ export type PolyglotFlow = PolyglotFlowInfo & {
   edges: PolyglotEdge[];
 };
 
-export enum TypeOfExercise {
-  fill_in_the_blanks,
-  question,
-  choice,
-  conceptual,
-  practical,
-}
-
-export type AnalyseType = { material: string };
-
-export type LOType = {
-  Topic: string;
-  Level: number;
-  Context: string;
+export type ProgressInfo = {
+  flowId: string;
+  userId: string;
 };
 
-export type MaterialType = {
-  topic: string;
-  numberOfWords: number;
-  level: number;
-  learningObjective: string;
+export type UserBaseInfo = {
+  ctx: {
+    ctxId: string;
+    username: string;
+    currentNode: string;
+    conditions: [{ edgeId: string; conditionKind: string }];
+  };
+  key: string;
 };
 
-export type SummarizeType = {
-  material: string;
-  numberOfWords: number;
-  level: number;
+export type PolyglotNodeValidation = PolyglotNode & {
+  validation: {
+    id: string;
+    title: string;
+    code: string;
+    data: any;
+    type: string;
+  }[];
 };
-
-export type AIExerciseType = {
-  macroSubject: string;
-  title: string;
-  level: number; //0=primary_school, 1=middle_school, 2=high_school, 3=college, 4=academy
-  typeOfActivity: TypeOfExercise; //0=fill_in_the_blanks, 1=question, 2=choice, 3=conceptual, 4=practical
-  learningObjective?: string;
-  bloomLevel: number; //0=Remembering, 1=Understanding, 2=Applying, 3=Analyzing, 4=Evaluating, 5=Creating
-  language: string;
-  material: string;
-  correctAnswersNumber?: number;
-  distractorsNumber?: number;
-  easilyDiscardableDistractorsNumber?: number;
-  assignmentType: number; //0=theoretical, 1=code, 2=problem_resolution,
-  topic: string;
-  temperature: number;
-};
-
-export type SummarizerBody = { lesson: string; noW: string; level: string };
