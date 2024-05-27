253d33c98d28ae11bff32a4090069e3150d00910
tmaog <tommasoguidolin01@gmail.com>
2024-05-27 17:53:56 +0200

    fix: prettier

--- src/data/api.ts
+++ src/data/api.ts
@@ -341,10 +341,7 @@ export const API = {
   },
 
   getActualNodeInfo: (body: { ctxId: string }): Promise<AxiosResponse> => {
-    return axios.post<{}, AxiosResponse, {}>(
-      `/api/execution/actual`,
-      body
-    );
+    return axios.post<{}, AxiosResponse, {}>(`/api/execution/actual`, body);
   },
 
   analyseMaterial: (body: AnalyseType): Promise<AxiosResponse> => {
