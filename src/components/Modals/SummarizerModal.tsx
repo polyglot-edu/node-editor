1b96776f99d0ff0f372c2fecc3129a3b5ab254a0
tmaog <121682498+tmaog@users.noreply.github.com>
2024-06-17 22:45:38 +0200

    log.console  remove

--- src/components/Modals/SummarizerModal.tsx
+++ src/components/Modals/SummarizerModal.tsx
@@ -41,7 +41,6 @@ const SummarizerModal = ({ isOpen, onClose }: ModelTemplateProps) => {
             marginTop={'5px'}
             onClick={async () => {
               try {
-                console.log("testing "+process.env.SETUPMODEL);
                 if (generateButton) {
                   toast({
                     title: 'Invalid syntax',
